e96db8351c544096d9df7020cb18f49fb03efe7e
Debangan Mana <194143546+DebanganRaj@users.noreply.github.com>
2025-09-11 00:28:36 +0530

    Update Register.jsx

--- Frontend/src/components/Register.jsx
+++ Frontend/src/components/Register.jsx
@@ -30,7 +30,7 @@ const Register = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/register", {
+      const response = await fetch("https://learn-x-grow-2-r7lg.onrender.com/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
