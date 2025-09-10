d8406479da2f55fd820c0148e4248918c3f9693d
Debangan Mana <194143546+DebanganRaj@users.noreply.github.com>
2025-09-11 00:27:40 +0530

    Update Login.jsx

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
     setError("");
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/login", {
+      const response = await fetch("https://learn-x-grow-2-r7lg.onrender.com/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username: email, password }),
