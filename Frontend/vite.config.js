e4f4f4de0aba8401967715aba51ee644662f02f8
Debangan Mana <194143546+DebanganRaj@users.noreply.github.com>
2025-09-11 00:25:09 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:5000',
+        target: 'https://learn-x-grow-2-r7lg.onrender.com',
         changeOrigin: true,
       },
     },
